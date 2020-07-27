9bab4ffdbbc821d268d09176aa4e8b31e3795eee
Jort van Gorkum <jortvangorkum@gmail.com>
2020-07-27 15:20:32 +0200

    Moved index.html

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/public/scripts'));
 app.use('/roms', express.static('roms'));
+app.use('/', function (req, res) { res.sendFile(path.join(__dirname, '../index.html')) })
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
